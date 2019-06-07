b32950871612064a1e5b24748601892d86ca747c
Andreas Prummer <prumand@gmail.com>
2019-06-07 11:07:35 +0200

    Remove further glutter

--- src/mergeMap.ts
+++ src/mergeMap.ts
@@ -12,21 +12,14 @@ export default function basicMergeMapObs(
     action$: Observable<Action>
 ) : Observable<any> {
     return action$.pipe(
-        ofType<Action, Test>('TEST'),
-        mergeMap((val: Action) => {
-            return of(val).pipe(
-                filter((val: Test) => {
-                    throw new Error('We stop here')
-                }),
-                (obs: Observable<any>) => of('test'), // WE_FINISH! + end
-                // (obs: Observable<any>) => obs, // MY_NEW_ERROR (no end!)
-                map((val: any) => ({
-                    type: 'WE_FINISH',
-                })),
-                catchError(() => of({
-                    type: 'MY_NEW_ERROR',
-                }))
-            )
-        })
+        filter((val: Test) => {
+            throw new Error('We stop here')
+        }),
+        map((val: any) => ({
+            type: 'WE_FINISH',
+        })),
+        catchError(() => of({
+            type: 'MY_NEW_ERROR',
+        }))
     )
 }
